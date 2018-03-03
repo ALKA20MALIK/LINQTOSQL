0c95961544edb67431f0fea311449411bb997e57
ALKAMALIK <malikalka2090>
2018-03-03 18:56:16 +0530

    LinqToSQL

---
+++ LINQDEMO/WebForm1.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="LINQDEMO.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <br />
+        <asp:GridView ID="GridView2" runat="server" OnSelectedIndexChanged="GridView2_SelectedIndexChanged">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ LINQDEMO/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQDEMO
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SampleDataContext DataContext = new SampleDataContext();
+            GridView1.DataSource = from s in DataContext.Students
+                                   where s.Gender == "Female"
+                                   select s;
+            GridView1.DataBind();
+
+           
+            GridView2.DataSource = ( from pd in DataContext.xtOrders
+                    join od in DataContext.xtSales on pd.OrderId equals od.OrderId
+                     orderby od.OrderId
+                     select new
+                     {                       
+                         pd.DepNo,
+                         pd.Amount,
+                         od.SalesManId
+                     }).ToList();
+            GridView2.DataBind();
+
+        }
+
+        protected void GridView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+    }
+}
